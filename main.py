ce7f3c4c8085eccf070bb990b0821d86fe3f3801
dima-starkov <62872531+dima-starkov@users.noreply.github.com>
2023-06-10 21:07:54 +0300

    add file

---
+++ main.py
@@ -0,0 +1,39 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5991103098:AAHlPTkB3pPEDL-63jIAAxBzCN2DSPC0b5g')
+
+managerContact = 'Ждy вашего сообщения @dima_starkov'
+
+helloMessage = 'Здравствуйте, если вас заинтересовали наши услуги, этот бот содержит ответы на ваши вопросы'
+
+req = '''📌Только граждане РФ🇷🇺,кроме Крыма и Кавказа\n
+📌Возраст от 23 лет🙍‍♂️\n
+📌Кредитный рейтинг 600+🔝\n
+📌Отсутствие просрочек и Микрозаймов🚫'''
+
+faq = '''🔸С какими банками мы работаем?\n
+ --Все крупные банки РФ: Сбербанк, ВТБ, Альфа-Банк\n
+ \n🔸Сколько времени занимает процесс?\n
+  --2-3 недели
+ '''
+
+@bot.message_handler(commands=['start'])
+def sendMessage(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Требования к заемщику', callback_data='requirements'))
+
+    markup.add(types.InlineKeyboardButton('Частые вопросы', callback_data='faq'))
+    markup.add(types.InlineKeyboardButton('Связаться с нами', callback_data='manager'))
+    bot.send_message(message.chat.id,helloMessage,reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'requirements':
+        bot.send_message(callback.message.chat.id, req)
+    elif callback.data == 'faq':
+        bot.send_message(callback.message.chat.id, faq)
+    elif callback.data == 'manager':
+        bot.send_message(callback.message.chat.id, managerContact)
+
+bot.polling(none_stop=True)
